6f0497038f8ec5f099371ea560c7143e9d6f6138
Ali El Deen <ali.eldeen008@gmail.com>
2025-06-11 18:45:08 +0300

    Added game route for mercury

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Landing/Home';
 import Galaxy from './components/Galaxy/Galaxy';
+import Mission001 from './components/Games/Mission001/Mission';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/galaxy" element={<Galaxy />} />
+        <Route path="/galaxy/mercury" element={<Mission001 />} />
       </Routes>
     </Router>
   );
